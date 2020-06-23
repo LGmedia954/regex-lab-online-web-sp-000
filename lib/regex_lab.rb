4adac8360caa7b13379b2807ead740bf518dd083
Leslie G <lesliegonzalez@me.com>
2020-06-23 13:15:15 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,5 @@ text.match(/^[A-Z].+[.?!]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-phone.scan(/(?:\d{1}\s)?\(?(\d{3})\)?-?\s?(\d{3})-?\s?(\d{4})/g) ? true : false
+phone.scan(/(?:\d{1}\s)?\(?(\d{3})\)?-?\s?(\d{3})-?\s?(\d{4})/g/) ? true : false
 end
