73ee6ef10cc66e1e1185d76de931aad344d1c2aa
Leslie G <lesliegonzalez@me.com>
2020-06-23 13:10:22 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,5 @@ text.match(/^[A-Z].+[.?!]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-phone.scan(/\d\a+[0-9]|\D\a+[^0-9]/) ? true : false
+phone.scan(/\d\a+[0-9]|.+a+[^0-9]/) ? true : false
 end
